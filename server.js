42237a018977fc9c9632046b34a55e03dfb3897b
wentdavid <davidwent@me.com>
2022-10-22 16:01:25 +0200

    imported url module

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+// importing url module
+const url = require('url');
+let addr = 'http://localhost:8080/default.html?year=2017&month=february';
+let q = url.parse(addr, true);
+
+console.log(q.host); // returns 'localhost:8080'
+console.log(q.pathname); // returns '/default.html'
+console.log(q.search); // returns '?year=2017&month=february'
+
+let qdata = q.query; // returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); // returns 'february'
+
+
+//importing html module
 const http = require('http');
 
 http.createServer((request, response) => {
