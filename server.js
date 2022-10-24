5d7912f7283e7abcb165fafd9938b55009c40d98
wentdavid <davidwent@me.com>
2022-10-24 12:53:57 +0200

    logging timestamps

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ console.log(q.search); // returns everything after the .html
  //http module
  const http = require('http');
 
- http.createServer((request, response) => {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello Node!\n');
+ http.createServer((request, response) => { 
    
    if (q.pathname.includes('documentation')) {
     filePath = (__dirname + '/documentation.html');
@@ -30,6 +28,48 @@ console.log(q.search); // returns everything after the .html
     response.end();
 
   });
+
+}).listen(8080);
+ 
+console.log('My first Node test server is running on Port 8080.');
+
+
+  //logging timestamps
+  const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
+
+http.createServer((request, response) => {
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+
+  });
+
+  
  }).listen(8080);
  
  console.log('My first Node test server is running on Port 8080.');
