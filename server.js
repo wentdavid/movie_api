a54bfc277af4f61b47a3e1fd1fc019f3c721e859
wentdavid <davidwent@me.com>
2022-10-24 12:34:06 +0200

    importing http module

--- server.js
+++ server.js
@@ -1,85 +1,9 @@
-// importing url module
-const url = require('url'); // Checks for the documentation html
-let addr = request.url;
-let q = new URL(addr, true);
 
-console.log(q.host); // returns 'localhost:8080'
-console.log(q.pathname); // returns '/#.html'
-console.log(q.search); // returns 'what that comes after the html file name
-
-// fs module
-const fs = require("fs");
-
-fs.readFile('input.txt', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log('File content: ' + data.toString());
-});
-
-//importing html module
-const http = require('http');
-
-http.createServer((request, response) => {
-    let addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
-
-    if (q.pathname.includes('documentation')) {
-        filePath = (__dirname + '/documentation.html');
-      } else {
-        filePath = 'index.html';
-      }
-    
-      fs.readFile(filePath, (err, data) => {
-        if (err) {
-          throw err;
-        }
-    
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(data);
-        response.end();
-    
-      });
-    
-    }).listen(8080);
-    console.log('My test server is running on Port 8080.');
-
-
-// log url timestamps
-const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
-
-http.createServer((request, response) => {
-  let addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
-
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Added to log.');
-    }
-  });
-
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html';
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      throw err;
-    }
-
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
-
-  });
-
-}).listen(8080);
-console.log('My test server is running on Port 8080.');
+ 
+ //http module
+ http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello Node!\n');
+  }).listen(8080);
+  
+  console.log('My first Node test server is running on Port 8080.');
