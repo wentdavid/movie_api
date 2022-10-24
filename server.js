b160fd53a77ccfa200d6cf208636f4d1b699bfc5
wentdavid <davidwent@me.com>
2022-10-24 12:45:41 +0200

    importing http module

--- server.js
+++ server.js
@@ -8,24 +8,13 @@ console.log(q.pathname); // returns '/#.html'
 console.log(q.search); // returns everything after the .html
  
  //http module
- const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
+ const http = require('http');
 
-http.createServer((request, response) => {
-  let addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
-
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Added to log.');
-    }
-  });
-
-  if (q.pathname.includes('documentation')) {
+ http.createServer((request, response) => {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello Node!\n');
+   
+   if (q.pathname.includes('documentation')) {
     filePath = (__dirname + '/documentation.html');
   } else {
     filePath = 'index.html';
@@ -41,7 +30,8 @@ http.createServer((request, response) => {
     response.end();
 
   });
+ }).listen(8080);
+ 
+ console.log('My first Node test server is running on Port 8080.');
 
-}).listen(8080);
-console.log('My test server is running on Port 8080.');
   
