c0d9a5ea33b7f2a4e64e4efc04a7c259cac244f8
wentdavid <davidwent@me.com>
2022-11-16 12:54:27 +0100

    eddit add users

--- index.js
+++ index.js
@@ -172,18 +172,18 @@ app.post('/users', (req, res) => {
       if (user) {
         return res.status(400).send(req.body.Username + 'already exists');
       } else {
-        Users
-          .create({
+        Users.create({
             Username: req.body.Username,
             Password: req.body.Password,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
+          .then((user) => {res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
       }
     })
     .catch((error) => {
