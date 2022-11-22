4b0cc66a1e9b35c609e7adc2bb9b7e177b872689
wentdavid <davidwent@me.com>
2022-11-22 16:46:14 +0100

    Integrating CORS

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ const express = require('express'),
 //BodyParser middleware function
 app.use(bodyParser.urlencoded({ extended: true}));
 
+//Integrating CORS and allow requests from ALL Origins
+const cors = require("cors");
+app.use(cors());
+
+//Integrating CORS and allow requets from only CERTAIN origins
+let allowedOrigins = ["http://localhost:8080", "htttp://testsite.com"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = "The CORS policy for this application doesn’t allow access from origin " + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 //App argument ensures that Express is available in your “auth.js” file as well.
 let auth = require("./auth")(app);
 
