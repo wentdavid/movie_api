39811169d1b4f10953404e6d1227c2275bbc9c26
wentdavid <davidwent@me.com>
2022-11-22 18:01:03 +0100

    replacing app.listen with a dynamic port

--- index.js
+++ index.js
@@ -478,10 +478,10 @@ app.get('/movies/directors/:Name', (req, res) => {
 });
 
 
-
-
-  // listen for requests
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+// listen for requests
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});
+  
   
