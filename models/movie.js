e520e49f9663500f0769978292c8f148b7122aeb
wentdavid <davidwent@me.com>
2023-01-12 12:46:39 +0100

    commenting

--- models/movie.js
+++ models/movie.js
@@ -1,19 +1,23 @@
+/* This file exports a Mongoose model for the Movie schema.
+It includes the fields for title, description, genre, director, actors, imageUrl, and featured.
+ */
+
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true },
-  description: { type: String, required: true, trim: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
   genre: {
-    name: { type: String, trim: true },
-    description: { type: String, trim: true },
+    name: String,
+    description: String,
   },
   director: {
-    name: { type: String, trim: true },
-    bio: { type: String, trim: true },
+    name: String,
+    bio: String,
     birth: Date,
   },
-  actors: [{ type: String, trim: true }],
-  imageUrl: { type: String, trim: true },
+  actors: [String],
+  imageUrl: String,
   featured: Boolean,
 });
 
