a70cf55a3f09582222ad6825a04c9f56f21b7dbb
wentdavid <davidwent@me.com>
2023-03-08 14:24:51 +0100

    add comments and generate JSDoc

--- auth.js
+++ auth.js
@@ -1,27 +1,43 @@
-const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+/**
+ * The secret key used in the JWTStrategy for user authentication.
+ * @type {string}
+ */
+const jwtSecret = "your_jwt_secret";
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+require("./passport"); // Your local passport file
 
-require('./passport'); // Your local passport file
-
-
+/**
+ * This method generates a JWT token for user authentication.
+ * @name generateJWTToken
+ * @function
+ * @param {object} user - The user object to be encoded in the JWT.
+ * @returns {string} Returns a JWT token.
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username you’re encoding in the JWT
-    expiresIn: '30d', // This specifies that the token will expire in 30 days
-    algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+    expiresIn: "30d", // This specifies that the token will expire in 30 days
+    algorithm: "HS256", // This is the algorithm used to “sign” or encode the values of the JWT
   });
-}
+};
 
-
-/* POST login. */
+/**
+ * This method authenticates the user and generates a JWT token for login.
+ * @name login
+ * @function
+ * @param {object} router - The router object to handle HTTP requests.
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @returns {object} Returns the user and JWT token if successful, or an error message if not.
+ */
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
+          message: "Something is not right",
           user: user,
         });
       }
@@ -32,7 +48,6 @@ module.exports = (router) => {
         let token = generateJWTToken(user.toJSON());
         return res.json({ user, token });
       });
-    }
-    )(req, res);
+    })(req, res);
   });
 };
--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-//decalring const
+/**
+ * Initializes Express and imports necessary modules
+ * @module app
+ * @requires express
+ * @requires morgan
+ * @requires fs
+ * @requires path
+ * @requires body-parser
+ * @requires uuid
+ * @requires method-override
+ * @requires cors
+*/
 const express = require("express"),
   morgan = require("morgan"),
-  fs = require("fs"), // import built in node modules fs and path
-  path = require("path"),
+  fs = require("fs"), // built-in Node.js module
+  path = require("path"), // built-in Node.js module
   app = express(),
   bodyParser = require("body-parser"),
   uuid = require("uuid"),
   methodOverride = require("method-override"),
   cors = require("cors");
 
-// create a write stream (in append mode)
-// a ‘log.txt’ file is created in root directory
+/**
+ * Create a write stream to log requests to a file called 'log.txt' in the root directory
+ * @const {Object} accessLogStream
+ */
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
-app.use(morgan("combined", { stream: accessLogStream }));
+app.use(morgan("combined", { stream: accessLogStream })); // use Morgan middleware to log all requests to the 'log.txt' file
 app.use(bodyParser.json());
-// app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: true })); // use bodyParser middleware to parse incoming request bodies
 
-//BodyParser middleware function
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Integrating CORS and allow requets from only CERTAIN origins
+/**
+ * Define the origins that are allowed to make requests to the API using the CORS middleware
+ * @const {Array} allowedOrigins
+ */
 let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
@@ -30,15 +43,14 @@ let allowedOrigins = [
   "https://sheltered-crag-54265.herokuapp.com/",
   "https://gleans-moviedb.netlify.app",
   "https://wentdavid.github.io/myFlix-Angular-client",
-  "https://wentdavid.github.io"
+  "https://wentdavid.github.io",
 ];
-
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
+      if (!origin) return callback(null, true); // allow requests with no origin
       if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
+        // if a specific origin isn’t found on the list of allowed origins
         let message =
           "The CORS policy for this application doesn’t allow access from origin " +
           origin;
@@ -49,18 +61,37 @@ app.use(
   })
 );
 
-//App argument ensures that Express is available in your “auth.js” file as well.
+/**
+ * Require auth module, passing app as an argument to ensure Express is available in "auth.js" file
+ * @const {Object} auth
+ */
 const auth = require("./auth")(app);
 
-//Requireing the Passport module and importing the “passport.js”
+/**
+ * Require Passport module and import passport.js file
+ * @const {Object} passport
+ */
 const passport = require("passport");
 app.use(passport.initialize());
 require("./passport");
 
-//Express-Validator
+/**
+ * Require and configure Express-Validator
+ * @const {Object} check
+ * @const {Function} validationResult
+ */
 const { check, validationResult } = require("express-validator");
 
-//Integrating Mongoose
+/**
+ * Require and configure Mongoose and import models.js file
+ * @const {Object} mongoose
+ * @const {Object} Models
+ * @const {Object} Movies
+ * @const {Object} Users
+ * @const {Object} Genres
+ * @const {Object} Directors
+ */
+
 const mongoose = require("mongoose"),
   Models = require("./models.js");
 
@@ -69,12 +100,10 @@ const Movies = Models.Movie,
   Genres = Models.Genre,
   Directors = Models.Director;
 
-/* mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); // allows Mongoose to connect to the local database
+/**
+ * Connect to MongoDB database using a secure connection URI
+ * @function
  */
-
-// process.env.CONNECTION_URI = 'mongodb://localhost:27017/myFlixDB';
-
-//for online database process.env.Variable name ro secure connection URI
 mongoose
   .connect(process.env.CONNECTION_URI, {
     useNewUrlParser: true,
@@ -82,24 +111,45 @@ mongoose
   })
   .then(console.log("DB Connected")); // if it successfully connects
 
-// setup the logger
+/**
+ * Use Morgan middleware to log all requests to a file called 'log.txt' in the root directory
+ * Serve static files from the 'public' folder
+ * @function
+ * @param {string} "common" - format of logged messages
+ * @param {Object} { stream: accessLogStream } - stream object specifying the file to log to
+ */
 app.use(morgan("common", { stream: accessLogStream }));
 app.use(express.static("public"));
 
-//default text response
+/**
+ * Responds with default text message when the root URL is accessed
+ * @function
+ * @param {Object} req - the HTTP request object
+ * @param {Object} res - the HTTP response object
+ */
+
 app.get("/", (req, res) => {
   res.send("Welcome to my Movie API!");
 });
 
+/**
+ * Sends the 'documentation.html' file from the 'public' folder when the '/documentation' URL is accessed
+ * @function
+ * @param {Object} req - the HTTP request object
+ * @param {Object} res - the HTTP response object
+ */
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
 });
 
-// Get all movies
-/*  app.get('/movies', (req, res) => {
-    res.json(movies);
-  }); */
-
+/**
+ * Responds with all movies in the database
+ * @function
+ * @param {string} "/movies" - the URL path for this endpoint
+ * @param {function} passport.authenticate - authenticate requests using JWT token
+ * @param {Object} req - the HTTP request object
+ * @param {Object} res - the HTTP response object
+ */
 app.get(
   "/movies",
   passport.authenticate("jwt", { session: false }),
@@ -115,7 +165,14 @@ app.get(
   }
 );
 
-// get movies by title
+/**
+ * Responds with a movie that matches the given title
+ * @function
+ * @param {string} "/movies/:title" - the URL path for this endpoint with a title parameter
+ * @param {function} passport.authenticate - authenticate requests using JWT token
+ * @param {Object} req - the HTTP request object with title parameter in URL
+ * @param {Object} res - the HTTP response object
+ */
 app.get(
   "/movies/:title",
   passport.authenticate("jwt", { session: false }),
@@ -131,7 +188,14 @@ app.get(
   }
 );
 
-//Get genre by Name
+/**
+ * Responds with all movies that match the given genre name
+ * @function
+ * @param {string} "/movies/genres/:Name" - the URL path for this endpoint with a Name parameter
+ * @param {function} passport.authenticate - authenticate requests using JWT token
+ * @param {Object} req - the HTTP request object with Name parameter in URL
+ * @param {Object} res - the HTTP response object
+ */
 app.get(
   "/movies/genres/:Name",
   passport.authenticate("jwt", { session: false }),
@@ -147,7 +211,14 @@ app.get(
   }
 );
 
-//Get director data by Name
+/**
+ * Responds with the director data that matches the given name
+ * @function
+ * @param {string} "/movies/directors/:Name" - the URL path for this endpoint with a Name parameter
+ * @param {function} passport.authenticate - authenticate requests using JWT token
+ * @param {Object} req - the HTTP request object with Name parameter in URL
+ * @param {Object} res - the HTTP response object
+ */
 app.get(
   "/movies/directors/:Name",
   passport.authenticate("jwt", { session: false }),
@@ -163,12 +234,18 @@ app.get(
   }
 );
 
-// Get all user (Read in Mongoose)
+/**
+ * Responds with all users in the databas
+ * @function
+ * @param {string} "/users" - the URL path for this endpoint
+ * @param {function} passport.authenticate - authenticate requests using JWT token
+ * @param {Object} req - the HTTP request object
+ * @param {Object} res - the HTTP response object
+ */
 app.get(
   "/users",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    //app.get("/users", function (req, res) {
     Users.find()
       .then((users) => {
         res.status(200).json(users);
@@ -180,7 +257,14 @@ app.get(
   }
 );
 
-//Get a user by username
+/**
+ * Responds with the user that matches the given username
+ * @function
+ * @param {string} "/users/:Username" - the URL path for this endpoint with a Username parameter
+ * @param {function} passport.authenticate - authenticate requests using JWT token
+ * @param {Object} req - the HTTP request object with Username parameter in URL
+ * @param {Object} res - the HTTP response object
+ */
 app.get(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
@@ -196,27 +280,39 @@ app.get(
   }
 );
 
-//Error
+/**
+ * Express error handler middleware that logs the error stack and responds with a 500 status and message
+ * @function
+ * @param {Object} err - the error object
+ * @param {Object} req - the HTTP request object
+ * @param {Object} res - the HTTP response object
+ * @param {function} next - the next middleware function in the chain
+ */
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
-// Allow new users register
+/**
+ * Allows new users to register by creating a new user document in the database
+ * @function
+ * @param {string} "/users" - the URL path for this endpoint
+ * @param {Array} [check(...)] - an array of middleware functions for validating the request parameters
+ * @param {Object} req - the HTTP request object with user data in the request body
+ * @param {Object} res - the HTTP response object
+ */
 app.post(
   "/users",
-  //Validation logic here for request
   [
-    check("Username", "Username is required").isLength({ min: 5 }), //username must be at least 5 chars long
+    check("Username", "Username is required").isLength({ min: 5 }),
     check(
       "Username",
       "Username contains non alphanumeric characters - not allowed."
-    ).isAlphanumeric(), //username must be alphanumeric
-    check("Password", "Password is required").not().isEmpty(), //password must be at least 5 chars long
-    check("Email", "Email does not appear to be valid").isEmail(), //email must be valid
+    ).isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
   ],
   (req, res) => {
-    //check the validation object for errors
     let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -224,10 +320,9 @@ app.post(
     }
 
     let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
-          //If the user is found, send a response that it already exists
           return res.status(400).send(req.body.Username + "already exists");
         } else {
           Users.create({
@@ -252,7 +347,15 @@ app.post(
   }
 );
 
-// Allow users update their user info (Update)
+/**
+ * Allows users to update their user information in the database
+ * @function
+ * @param {string} "/users/:Username" - the URL path for this endpoint with a Username parameter
+ * @param {function} passport.authenticate - authenticate requests using JWT token
+ * @param {Array} [check(...)] - an array of middleware functions for validating the request parameters
+ * @param {Object} req - the HTTP request object with user data in the request body and Username parameter in URL
+ * @param {Object} res - the HTTP response object
+ */
 app.put(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
@@ -283,52 +386,6 @@ app.put(
           Birthday: req.body.Birthday,
         },
       },
-      { new: true }, //This line makes sure that the updated document is returned
-      (err, updatedUser) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error: " + err);
-        } else {
-          res.json(updatedUser);
-        }
-      }
-    );
-  }
-);
-
-// Allow users add to their list of Favorites (create)
-app.post(
-  "/users/:Username/movies/:MovieID",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      {
-        $push: { FavoriteMovies: req.params.MovieID },
-      },
-      { new: true }, //This line makes sure that the updated document is returned
-      (err, updatedUser) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error: " + err);
-        } else {
-          res.json(updatedUser);
-        }
-      }
-    );
-  }
-);
-
-//Delete movie from favorite list
-app.delete(
-  "/users/:Username/movies/:MovieID",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      {
-        $pull: { FavoriteMovies: req.params.MovieID },
-      },
       { new: true },
       (err, updatedUser) => {
         if (err) {
@@ -342,7 +399,86 @@ app.delete(
   }
 );
 
-//Allow existing users deregister
+/**
+ * Allow users to add a movie to their list of favorites.
+ * @name POST/users/:Username/movies/:MovieID
+ * @function
+ * @memberof module:routers/users~usersRouter
+ * @inner
+ * @param {string} req.params.Username - The username of the user.
+ * @param {string} req.params.MovieID - The ID of the movie to be added to the user's favorites.
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @param {function} next - The Express middleware function.
+ * @returns {Object} The updated user object with the newly added movie in their favorite movies list.
+ */
+app.post(
+  "/users/:Username/movies/:MovieID",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      {
+        $push: { FavoriteMovies: req.params.MovieID },
+      },
+      { new: true }, //This line makes sure that the updated document is returned
+      (err, updatedUser) => {
+        if (err) {
+          console.error(err);
+          return next(err);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+
+/**
+ * Allow users to delete a movie from their list of favorites.
+ * @name DELETE/users/:Username/movies/:MovieID
+ * @function
+ * @memberof module:routers/users~usersRouter
+ * @inner
+ * @param {string} req.params.Username - The username of the user.
+ * @param {string} req.params.MovieID - The ID of the movie to be removed from the user's favorites.
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @param {function} next - The Express middleware function.
+ * @returns {Object} The updated user object with the removed movie from their favorite movies list.
+ */
+app.delete(
+  "/users/:Username/movies/:MovieID",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      {
+        $pull: { FavoriteMovies: req.params.MovieID },
+      },
+      { new: true },
+      (err, updatedUser) => {
+        if (err) {
+          console.error(err);
+          return next(err);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+
+/**
+ * This method allows existing users to be deregistered from the application.
+ * @name deleteUsers
+ * @function
+ * @memberof app
+ * @param {string} req.params.Username - The username of the user to be deleted.
+ * @param {function} passport.authenticate - Passport authentication middleware.
+ * @param {object} res - Express response object.
+ * @returns {string} Returns a status message indicating success or failure.
+ */
 app.delete(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
@@ -362,7 +498,16 @@ app.delete(
   }
 );
 
-// Confirm Updates via password verification
+/**
+ * This method verifies the user's password before allowing updates to be made.
+ * @name verifyPassword
+ * @function@memberof app
+ * @param {string} req.body.username - The username of the user.
+ * @param {string} req.body.password - The password entered by the user.
+ * @param {function} passport.authenticate - Passport authentication middleware.
+ * @param {object} res - Express response object.
+ * @returns {string} Returns a success or error message.
+ */
 app.post(
   "/verify-password",
   passport.authenticate("jwt", { session: false }),
@@ -386,13 +531,30 @@ app.post(
   }
 );
 
+/**
+ * This method serves the documentation HTML file.
+ * @name documentation
+ * @function
+ * @memberof app
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @returns {object} Returns the documentation HTML file.
+ */
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
 });
 
-// listen for requests
+/**
+ * This method listens for requests on the specified port.
+ * @name listen
+ * @function
+ * @memberof app
+ * @param {number} port - The port to listen on.
+ * @param {string} "0.0.0.0" - The IP address to listen on.
+ * @param {function} callback - Callback function to be called once the server is listening.
+ * @returns {string} Returns a console log message indicating the server is listening.
+ */
 const port = process.env.PORT || 8080;
-
 app.listen(port, "0.0.0.0", () => {
   console.log("Listening on Port " + port);
 });
